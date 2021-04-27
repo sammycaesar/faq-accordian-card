abdcd3b5d9461f04f10d892fedb8f7ca640a8fe6
sammycaesar <samantha.caesar@bigpond.com>
2021-04-27 17:45:25 +0200

    updates

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
 "use strict";
 
-//Variables
-let questionClicked = document.getElementById("question");
+// // Variables;
+// const questionClicked = document.querySelectorAll("#question");
+// const displayAnswer = document.getElementsByClassName("accordion-answer");
 
-//When user triggers a question
+// //When user triggers a question, answer is revealed
+// document.addEventListener("click", (e) => {
+//   this.classList.add("accordian-answer");
+//   if (displayAnswer.style.maxHeight) {
+//     displayAnswer.style.maxHeight = null;
+//   } else {
+//     displayAnswer.style.maxHeight = displayAnswer.scrollHeight + "px";
+//   }
+// });
+
+// Collapses a expanded question
+
+var questionClicked = document.querySelectorAll("#question");
+let displayAnswer = document.getElementsByClassName("accordion-answer");
+var i;
+
+for (i = 0; i < questionClicked.length; i++) {
+  questionClicked[i].addEventListener("click", (e) => {
+    console.log("Question clicked");
+
+    if (displayAnswer.style.display === "block") {
+      displayAnswer.style.display = "none";
+      this.classList.remove("accordian-answer");
+    } else {
+      displayAnswer.style.display = "block";
+      this.classList.toggle("accordian-answer");
+    }
+  });
+}
